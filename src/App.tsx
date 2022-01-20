6a1485932780f5a04deb9f26d1b7c5fc74592109
Anna Cunnane <43313455+Moggach@users.noreply.github.com>
2022-01-20 16:46:01 +0000

    installing tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css';
 import Card from './components/Card';
 import Illustration from './images/illustration.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">My recovery</header>
-      <div className="body-text">
+    <div className="container mx-auto">
+      <header className="font-bold">My recovery</header>
+      <div className="font-bold">
         Phasellus nec rhoncus turpis, id lacinia nisi. Quisque at turpis et dui
         pretium mattis non iaculis ligula. Fusce vel ligula pretium, blandit
         turpis ut, finibus nunc. Morbi vitae purus euismod, tempus nisi nec,
