929cbf0b62a87e1c94d7783895a1f490e5ae500e
Anna Cunnane <43313455+Moggach@users.noreply.github.com>
2022-01-20 12:09:38 +0000

    adding illustration from undraw

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import './App.css';
+import illustration from './images/illustration.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">My recovery</header>
+      <div className="body-text">
+        Phasellus nec rhoncus turpis, id lacinia nisi. Quisque at turpis et dui
+        pretium mattis non iaculis ligula. Fusce vel ligula pretium, blandit
+        turpis ut, finibus nunc. Morbi vitae purus euismod, tempus nisi nec,
+        eleifend sem. Praesent rutrum dictum metus et vestibulum. Etiam
+        pellentesque dignissim ultrices.
+      </div>
+      <img src={illustration} alt="" />
     </div>
   );
 }
